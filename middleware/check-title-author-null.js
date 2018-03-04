234e3236371d05056487f41dd6a940c922581f3f
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-04 23:10:12 +0700

    check title and author not null

--- middleware/check-title-author-null.js
+++ middleware/check-title-author-null.js
@@ -1,7 +1,7 @@
 module.exports = function (request, response, next) {
     if (!request.body.title)
-        return response.status(400).json({message:'title must not null'});
+        return response.status(400).json({ message : 'title must not null' });
     if (!request.body.author)
-        return response.status(400).json({message:'author must not null'});
+        return response.status(400).json({ message : 'author must not null' });
     next();
 };
