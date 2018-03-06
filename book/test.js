8e0dfab621c4184e8ac9327a3780079bc7335fc0
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-06 09:39:46 +0700

    test file

--- book/test.js
+++ book/test.js
@@ -2,16 +2,34 @@ const knex                   = require('../databases/mysql-connection');
 const BookRepository         = require('./book-repository');
 const Book                   = require('./book');
 const BookFactory            = require('./book-factory');
+const Publisher              = require('../publisher/publisher');
 const KeywordSearchCondition = require('./../book-searching-service/keyword-search-condition');
 
-let bookRepo = new BookRepository(knex, new BookFactory());
-let book     = new Book("One Punch");
+let bookRepo  = new BookRepository(knex, new BookFactory());
+let publisher = new Publisher('tori ya');
+publisher.setId(1);
+publisher.setAddress('VP');
+publisher.setPhone('0988735492');
+let book      = new Book("One Punch");
+book.setAuthor('Tori ');
+book.setPublisher(publisher);
+book.setPrice(8.3);
 
-book.setId(11);
+/*bookRepo.add(book).then(function (result) {
+    console.log(result);
+});*/
 
-let keywordSearchCondition = new KeywordSearchCondition("a");
-console.log(keywordSearchCondition.describe(knex.select().from('books')).toSQL().sql);
+bookRepo.get(1).then(function (results) {
+    let book = results.map(function (result) {
+        return result;
+    });
+    console.log(book);
+});
 
-let query = knex.select().from('books');
-keywordSearchCondition.describe(query);
-console.log(query.toSQL().sql);
+//
+// let keywordSearchCondition = new KeywordSearchCondition("a");
+// console.log(keywordSearchCondition.describe(knex.select().from('books')).toSQL().sql);
+//
+// let query = knex.select().from('books');
+// keywordSearchCondition.describe(query);
+// console.log(query.toSQL().sql);
