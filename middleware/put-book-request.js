4504507ba683327e766732fffc97cf9eb7d6dd01
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-06 13:31:39 +0700

    put book request

--- middleware/put-book-request.js
+++ middleware/put-book-request.js
@@ -3,9 +3,10 @@ const Book = require('../book/book');
 module.exports = function (request, response, next) {
     let book = new Book(request.body.title);
     book.setAuthor(request.body.author);
-    book.setPublisher(request.body.publisher);
+    book.setPublisher(request.publisher);
     book.setPrice(request.body.price);
     book.setId(request.params.id);
     request.book = book;
+    console.log(book);
     next();
 };
