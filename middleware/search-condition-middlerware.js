ab78597511b42fa7191e65c4e1cc22b17542b235
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-06 17:25:54 +0700

    search condition middlerware

--- middleware/search-condition-middlerware.js
+++ middleware/search-condition-middlerware.js
@@ -1,5 +1,7 @@
-const AdvanceSearchCondition = require('./../book-searching-service/advance-search-condition');
-const KeywordSearchCondition = require('./../book-searching-service/keyword-search-condition');
+const AdvanceSearchCondition   = require('./../book-searching-service/advance-search-condition');
+const KeywordSearchCondition   = require('./../book-searching-service/keyword-search-condition');
+const IdSeatrchCondition       = require('./../book-searching-service/id-search-condition');
+const UndeletedSearchCondition = require('./../book-searching-service/undeleted-search-conditon');
 
 module.exports = (request, response, next) => {
     request.searchCondition = makeSearchCondition(request);
@@ -9,7 +11,11 @@ module.exports = (request, response, next) => {
 function makeSearchCondition(request) {
     if(request.path === '/search-advance') {
         return new AdvanceSearchCondition(request.query.title, request.query.author, request.query.publisher);
-    } else if (request.path === '/search-basic'){
+    } else if (request.path === '/search-basic') {
         return new KeywordSearchCondition(request.query.keyword);
+    } else if (request.path === '/books') {
+        return new UndeletedSearchCondition();
+    } else if (request.path.toString().startsWith('/book/')) {
+        return new IdSeatrchCondition(request.params.id);
     }
 }
