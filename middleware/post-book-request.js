189ccfc3979bfbc5bb53934ccbc982940c0c5bbc
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-06 13:30:07 +0700

    create object book

--- middleware/post-book-request.js
+++ middleware/post-book-request.js
@@ -3,7 +3,7 @@ const Book = require('../book/book');
 module.exports = function (request, response, next) {
     let book = new Book(request.body.title);
     book.setAuthor(request.body.author);
-    book.setPublisher(request.body.publisher);
+    book.setPublisher(request.publisher);
     book.setPrice(request.body.price);
     request.book = book;
     next();
