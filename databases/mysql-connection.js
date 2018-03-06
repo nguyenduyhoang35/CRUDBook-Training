ad1d912587b87ce78ffa90d7319e38d27200f577
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-06 09:41:10 +0700

    fix connection

--- databases/mysql-connection.js
+++ databases/mysql-connection.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client    :'mysql',
     connection:{
-        host    :'localhost',
-        user    :'root',
-        password:'123456',
-        database:'CRUDBook'
+        host     : 'localhost',
+        user     : 'root',
+        password : '123456',
+        database : 'CRUDBook'
     }
 });
 
