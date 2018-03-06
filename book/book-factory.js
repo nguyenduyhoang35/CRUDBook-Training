0ad815809fdf76b1f1ab7086dd75168463655963
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-06 09:42:04 +0700

    add publishers

--- book/book-factory.js
+++ book/book-factory.js
@@ -1,4 +1,5 @@
 const Book = require('./book');
+const Publisher = require('../publisher/publisher');
 
 class BookFactory {
 
@@ -9,8 +10,10 @@ class BookFactory {
      */
     make(bookRawData) {
         let book = new Book(bookRawData.title);
+        let publisher = new Publisher(bookRawData.name);
+        publisher.setId(bookRawData.publisher_id);
         book.setAuthor(bookRawData.author);
-        book.setPublisher(bookRawData.publisher);
+        book.setPublisher(publisher);
         book.setPrice(bookRawData.price);
         book.setId(bookRawData.id);
         return book;
