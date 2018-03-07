2a07fab727ef29e07eebf0229c96057e86ff1f12
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-08 00:54:49 +0700

    put book request make book from request

--- middleware/put-book-request.js
+++ middleware/put-book-request.js
@@ -1,12 +1,10 @@
-const Book = require('../book/book');
 
 module.exports = function (request, response, next) {
-    let book = new Book(request.body.title);
-    book.setAuthor(request.body.author);
-    book.setPublisher(request.publisher);
-    book.setPrice(request.body.price);
-    book.setId(request.params.id);
-    request.book = book;
-    console.log(book);
-    next();
+    request.app.get('bookFactory').makeFromRequest(request.body)
+        .then(function (book) {
+            book.setId(request.params.id);
+            request.book = book;
+            next();
+        });
+    //the deo nao lai co rac trong code the nay????
 };
