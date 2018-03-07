c9fc31a4b60fd37c6e1e24e8180ea06f08c03ac6
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-08 00:51:04 +0700

    fix not update edit

--- controllers/bookcontroller.js
+++ controllers/bookcontroller.js
@@ -4,16 +4,6 @@ class BookController {
 
     }
 
-    getAll(request, response) {
-        let bookRepo = request.app.get('books');
-        bookRepo.all().then(function (books) {
-            let results = books.map(function (element) {
-                return element.toJson();
-            });
-            response.status(200).send(results);
-        });
-    }
-
     createBook(request, response, next) {
         let repo = request.app.get('books');
         repo.add(request.book).then(function (result) {
@@ -34,16 +24,6 @@ class BookController {
         let repo = request.app.get('books');
         repo.edit(request.book).then(function () {
             response.status(200).json({message:'Success'});
-        });
-    }
-
-    searchBook(request, response) {
-        let repo = request.app.get('books');
-        repo.get(request.params.id).then(function (books) {
-            let bookResults = books.map(function (book) {
-                return book.toJson();
-            });
-            response.status(200).send(bookResults);
         });
     }
 
