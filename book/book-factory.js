9cc8f02d25ecaae308a5d22ef16f4dba1f2982d8
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-08 00:11:56 +0700

    add makeFromRequest

--- book/book-factory.js
+++ book/book-factory.js
@@ -1,5 +1,9 @@
-const Book      = require('./book');
-const Publisher = require('../publisher/publisher');
+const Book              = require('./book');
+const Publisher         = require('../publisher/publisher');
+const PublisherProvider = require('../publisher/publisher-provider');
+const knexConnection    = require('../publisher/publisher-provider');
+
+let publisherProvider   = new PublisherProvider();
 
 class BookFactory {
 
@@ -8,7 +12,7 @@ class BookFactory {
      * @param {object} bookRawData
      * @return {Book}
      */
-    make(bookRawData) {
+    makeFromRowData(bookRawData) {
         let book = new Book(bookRawData.title);
         book.setAuthor(bookRawData.author);
         book.setPrice(bookRawData.price);
@@ -17,6 +21,22 @@ class BookFactory {
         publisher.setId(bookRawData.publisher_id);
         book.setPublisher(publisher);
         return book;
+    }
+
+    /**
+     *
+     * @param {object}requestBody
+     * @return {Book}
+     */
+    makeFromRequest(requestBody) {
+        let book = new Book(requestBody.title);
+        book.setPrice(requestBody.price);
+        book.setAuthor(requestBody.author);
+        return publisherProvider.provide(requestBody.publisher_id)
+            .then(function(publisher) {
+                book.setPublisher(publisher);
+                return book;
+            });
     }
 }
 
