b2e6aeecb84630b78050918a832ef32035d64d24
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-07 14:12:48 +0700

    require dotenv config

--- databases/mysql-connection.js
+++ databases/mysql-connection.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const knex = require('knex')({
-    client    :'mysql',
-    connection:{
-        host     : 'localhost',
-        user     : 'root',
-        password : '123456',
-        database : 'CRUDBook'
+    client    : 'mysql',
+    connection: {
+        host     : process.env.DB_HOST ,
+        user     : process.env.DB_USER ,
+        password : process.env.DB_PASS ,
+        database : process.env.DB_NAME
     }
 });
 
