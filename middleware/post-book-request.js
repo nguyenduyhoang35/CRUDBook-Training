8dcc8cac8d39e43128d11ac8b7731fab057c5297
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-08 00:11:10 +0700

    make book from request body

--- middleware/post-book-request.js
+++ middleware/post-book-request.js
@@ -1,10 +1,7 @@
-const Book = require('../book/book');
-
 module.exports = function (request, response, next) {
-    let book = new Book(request.body.title);
-    book.setAuthor(request.body.author);
-    book.setPublisher(request.publisher);
-    book.setPrice(request.body.price);
-    request.book = book;
-    next();
+    request.app.get('bookFactory').makeFromRequest(request.body)
+        .then(function (book) {
+        request.book = book;
+        next();
+    });
 };
