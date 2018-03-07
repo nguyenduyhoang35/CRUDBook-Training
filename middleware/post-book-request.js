4f59fe41950b109895d6df74cb42a184641a17f6
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-08 00:53:46 +0700

    make book from request

--- middleware/post-book-request.js
+++ middleware/post-book-request.js
@@ -1,7 +1,7 @@
 module.exports = function (request, response, next) {
-    request.app.get('bookFactory').makeFromRequest(request.body)
-        .then(function (book) {
-        request.book = book;
-        next();
+     request.app.get('bookFactory').makeFromRequest(request.body)
+         .then(function (book) {
+             request.book = book;
+             next();
     });
 };
