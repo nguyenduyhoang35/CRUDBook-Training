08a2d51fefeb217630ed58f4e4fd2e21bbb43a72
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-08 11:14:23 +0700

    clean code

--- publisher/publisher-provider.js
+++ publisher/publisher-provider.js
@@ -9,15 +9,20 @@ class PublisherProvider {
     /**
      *
      * @param {int} publisher_id
+     * @return {Publisher} publisher
      */
     provide(publisher_id) {
-        return this.connection.select().from('publishers').where({id : publisher_id}).then(function (publisherRowData) {
-            let publisher = new Publisher(publisherRowData[0].name);
-            publisher.setAddress(publisherRowData[0].address);
-            publisher.setPhone(publisherRowData.phone_number);
-            publisher.setId(publisher.id);
-            return publisher;
-        });
+        return this.connection.select().from('publishers').where({id : publisher_id})
+            .then(function (publisherRowData) {
+                if (publisherRowData.length === 0) {
+                    return new Publisher('');
+                }
+                let publisher = new Publisher(publisherRowData[0].name);
+                publisher.setAddress(publisherRowData[0].address);
+                publisher.setPhone(publisherRowData[0].phone_number);
+                publisher.setId(publisherRowData[0].id);
+                return publisher;
+            });
     }
 }
 
