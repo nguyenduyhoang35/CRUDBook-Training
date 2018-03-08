b7aa31d7cd7cf825e5af297b791cf1402ef575b9
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-08 09:03:56 +0700

    fix message

--- middleware/check-length-title-author.js
+++ middleware/check-length-title-author.js
@@ -1,7 +1,8 @@
 module.exports = function(request, response, next) {
+    //message ngu nhu bo`
     if (request.body.title.length > 40)
-        return response.status(400).json({ message : 'length must < 40' });
+        return response.status(400).json({ message : 'title must < 40 character' });
     if (request.body.title.author > 100)
-        return response.status(400).json({ message : 'length must < 100' });
+        return response.status(400).json({ message : 'author must < 100 character'});
     next();
 };
