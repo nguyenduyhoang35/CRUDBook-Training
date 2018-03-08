40c49edba9d48a16f11ac54e86dbabbfd74a549c
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-08 09:06:06 +0700

    app set bookFactory

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const router         = require('./routers');
 const app            = express();
 const Factory        = require('./book/book-factory');
 const Searcher       = require('./book-searching-service/searcher');
+const BookFactory    = require('./book/book-factory');
 
 let bookFactory = new Factory();
 
-app.set('books', new BookRepository(knex, bookFactory));
+app.set('books', new BookRepository(knex));
+
+app.set('bookFactory', new BookFactory());
 
 app.set('book.searcher', new Searcher(knex, bookFactory));
 
