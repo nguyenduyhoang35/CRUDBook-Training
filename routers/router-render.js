9c5bc92a33cc5e91ffe99010d6029f5a606a4865
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-13 17:29:14 +0700

    router of render

---
+++ routers/router-render.js
@@ -0,0 +1,35 @@
+const SearchConditionMiddleware = require('../middleware/search-condition-middlerware');
+const BookController            = require('../controllers/book/book-controller');
+const Router                    = require('express').Router;
+const postBookRequest           = require('../middleware/post-book-request');
+const checkTitleAuthorNull      = require('../middleware/check-title-author-null');
+const checkTitleAuthorLength    = require('../middleware/check-length-title-author');
+const priceDefaultValue         = require('../middleware/price-default-value');
+
+let router              = new Router();
+let bookController      = new BookController();
+let middlewareOfPost = [checkTitleAuthorNull, checkTitleAuthorLength, priceDefaultValue, postBookRequest];
+
+router.get('/books', SearchConditionMiddleware, bookController.search);
+
+router.get('/book/:id', SearchConditionMiddleware, bookController.detail);
+
+router.get('/add', bookController.renderAddBook);
+
+router.post('/book', middlewareOfPost, bookController.createBook);
+
+router.get('/', function (req, res) {
+    res.render('home.ejs');
+    res.end();
+});
+
+router.get('/search-basic', SearchConditionMiddleware, bookController.search);
+
+router.get('/search-advance', SearchConditionMiddleware, bookController.search);
+
+router.get('/detail/:id', function (req, res) {
+    res.render('detail.ejs');
+    res.end();
+});
+
+module.exports = router;
