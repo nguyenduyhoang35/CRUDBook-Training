3109d3b8ec4814124e5819e06a4f9e5ece3b0fc8
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-15 09:26:35 +0700

    exporst router

--- routers/index.js
+++ routers/index.js
@@ -1,2 +1,2 @@
-exports.routerRender = require('./router-render');
-exports.routerApi    = require('./router-api');
+exports.router    = require('./router');
+exports.routerApi = require('./router-api');
