7cccedb3f80ca493e732bc9c9354864cd1532792
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-21 15:17:06 +0700

    ajax client get books

--- public/javascript/list-books.js
+++ public/javascript/list-books.js
@@ -1,13 +1,36 @@
 $(document).ready(function () {
     $.get('/books').then(renderBook);
 
+    $.get('/pages').then(renderPageNumber);
+
+    function renderPageNumber(pages) {
+        let template = $('#pageNumber').html();
+        let resultHtml = pages.map( (page) => {
+            return template.replace(':pageNumber:',page);
+        }).join('');
+        $('#page').html(resultHtml);
+
+    }
+
     function renderBook(books) {
         let template = $('#bookTemplate').html();
         let resultHtml = books.map( (book) => {
-            return template.replace(':bookName:', book.title).replace(':id:', book.id).replace(':id', book.id);
+            return template.replace(':bookName:', book.title)
+                .replace(':id:', book.id)
+                .replace(':book:id', book.id)
+                .replace(':author:', book.author);
         }).join('');
         $('#list-books').html(resultHtml);
     }
+
+    $('#form-search-basic').submit(function (event) {
+        event.preventDefault();
+    });
+
+    $('#search-basic-click').click(function () {
+
+        $('#form-search-basic').toggle();
+    });
 
     $('#search-basic').change(function () {
         let $this = $(this);
@@ -20,11 +43,13 @@ $(document).ready(function () {
        $('#search-advance').toggle();
     });
 
-    $('#advance-click').click(function () {
+    $('#search-advance').submit(function (event) {
+        event.preventDefault();
        $.get('/search-advance', {
            title : $('#title').val(),
            author : $('#author').val(),
            publisher : $('#publisher').val()
        }).then(renderBook);
     });
+
 });
